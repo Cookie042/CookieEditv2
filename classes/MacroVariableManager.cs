4ec5c81e014c82523deed919a1eda9e6d605ff8b
MONSTERCOOKE\carad <cara.di42@gmail.com>
2018-08-28 01:00:53 -0700

    Swith to enum

--- classes/MacroVariableManager.cs
+++ classes/MacroVariableManager.cs
@@ -19,63 +19,26 @@ namespace CookieEdit2
             }
         }
 
+        public enum ArgLetter
+        {
+            NULL = 0, A = 1, B = 2, C = 3, I = 4, J = 5, K = 6,
+            D = 7, E = 8, F = 9, H = 11, M = 13, Q = 17, R = 18,
+            S = 19, T = 20, U = 21, V = 22, W = 23, X = 24, Y = 25, Z = 26
+        }
+
         public string getArgLetter(int id)
         {
-            switch (id)
-            {
-                case 1:
-                    return "A";
-                case 2:
-                    return "B";
-                case 3:
-                    return "C";
-                case 4:
-                    return "I";
-                case 5:
-                    return "J";
-                case 6:
-                    return "K";
-                case 7:
-                    return "D";
-                case 8:
-                    return "E";
-                case 9:
-                    return "F";
-                case 11:
-                    return "H";
-                case 13:
-                    return "M";
-                case 17:
-                    return "Q";
-                case 18:
-                    return "R";
-                case 19:
-                    return "S";
-                case 20:
-                    return "T";
-                case 21:
-                    return "U";
-                case 22:
-                    return "V";
-                case 23:
-                    return "W";
-                case 24:
-                    return "X";
-                case 25:
-                    return "Y";
-                case 26:
-                    return "Z";
-                default:
-                    return "";
-            }
+            var enumString = ((ArgLetter) id).ToString();
 
+            if (!int.TryParse(enumString, out int r))
+                return enumString;
+            return "";
         }
     }
     
     
     public class MacroVariable
     {
-        
         public int id { get; set; }
         public string name { get; set; }
         public float value { get; set; }
